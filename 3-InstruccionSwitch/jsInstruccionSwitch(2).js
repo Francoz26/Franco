174d115415d5eff752086d65a4c52310da7655da
Francoz26 <60697943+Francoz26@users.noreply.github.com>
2020-02-12 12:25:58 -0300

    Add files via upload

---
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -0,0 +1,34 @@
+function mostrar()
+{
+//tomo la edad  
+var mesDelAnio = document.getElementById('mes').value;
+
+switch(mesDelAnio)
+{
+    case "Enero":
+    case "Febrero":
+    case "Marzo":
+    case "Abril":
+    case "Mayo":
+    case "Junio":
+    {
+        alert("Falta para el invierno");
+        break;
+    }
+    case "Julio":
+    case "Agosto":
+    {
+        alert("Abrigate que hace frío");
+        break;
+    }
+    default:
+    {
+        alert("¡¡¡Ya pasamos el frío, ahora calor!!!");
+        break;
+    }
+}
+
+
+
+
+}//FIN DE LA FUNCIÓN
